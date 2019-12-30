61e9820605505a4652831fa88a8ecc7c50c03d39
Daniel Rataj <daniel.rataj@mall.cz>
2019-12-30 08:39:07 +0100

    Add more unit tests

--- test/normalize-path.spec.js
+++ test/normalize-path.spec.js
@@ -16,6 +16,39 @@ describe('Normalize-path feature', function () {
     assert(_.isString(normalizePath(reqWithParams)))
   })
 
+  it('returns normalized string', function () {
+    assert(_.isString(normalizePath(reqWithParams, {
+      normalizePath: true
+    })))
+  })
+
+  it('returns normalized string with regex in path', function () {
+    assert(_.isString(normalizePath(reqWithParams, {
+      normalizePath: [
+        [/[a-z]/, '']
+      ]
+    })))
+
+    assert(_.isString(normalizePath(reqWithParams, {
+      normalizePath: [
+        ['[a-z]', '']
+      ]
+    })))
+
+    assert.throws(
+      () => {
+        normalizePath(reqWithParams, {
+          normalizePath: [
+            ['[a-z]', '', '']
+          ]
+        })
+      }, Error)
+
+    assert(_.isString(normalizePath(reqWithParams, {
+      urlValueParser: require('url-value-parser')
+    })))
+  })
+
   it('URL is the same', function () {
     assert.strictEqual(normalizePath(reqWithoutParams), reqWithoutParams.url)
   })
